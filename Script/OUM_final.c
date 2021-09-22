283c05791161d0c9887e1a02cebb5149cda9e634
erwanfadlys <78579279+erwanfadlys@users.noreply.github.com>
2021-09-22 18:50:56 +0800

    VER 1.3
    -Added return function for file pointer upon error.
    -Introducing trigger condition and exit condition.
    -Added average and total marks.
    -Added comment for most of the part
    -Cleaned the code to make it presentable.

--- Script/OUM_final.c
+++ Script/OUM_final.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-int num_students, num_courses, x, total, marks, course, student_evaluated; //x placed there for loop 
+int num_students, num_courses, x, y, marks, course, student_evaluated; //x placed there for loop 
 long int student_id; //to store numbers with 4 bytes
 char filename, export; //this is where the data name is stored
+float total, average_marks;
 
 void intro(){
     printf("SIMPLE GRADING SYSTEM V1.1\n\n");
@@ -12,22 +13,22 @@ void intro(){
 
 void marking(){ //marking function seperated from main, flexibility in coding the main func.
     if (marks >=80 && marks <=100){ 
-            printf("\t\t%s", "Grade: A\n\n");
+            printf("\t%s", "Grade: A\n\n");
         }
         else if (marks >= 70 && marks <= 79){ 
-            printf("\t\t%s", "Grade: B\n\n");
+            printf("\t%s", "Grade: B\n\n");
         }
         else if (marks >=60 && marks <=69){ 
-            printf("\t\t%s", "Grade: C\n\n");
+            printf("\t%s", "Grade: C\n\n");
         }
         else if (marks >=50 && marks <=59){
-            printf("\t\t%s", "Grade: D\n\n");
+            printf("\t%s", "Grade: D\n\n");
         }
         else if (marks >=0 && marks <=49){
-            printf("\t\t%s", "Grade: F\n\n");
+            printf("\t%s", "Grade: F\n\n");
         }
         else{
-            printf("\t\tMARK IS NOT GRADEABLE\n\n");
+            printf("\tMARK IS NOT GRADEABLE\n\n");
         }
 }
 
@@ -36,7 +37,7 @@ void data(){ //will not be hardcoded instead. Allow for future changes.
     scanf("%s", &filename);
 }
 
-void export_file(){  
+void export_file(){  //should this features inserted?? working on the implementation
     printf("\nExport the file as text?: y / n : ");
     scanf("%s", &export);
 
@@ -49,47 +50,57 @@ void export_file(){
     }
 }
 
+
 int main(){
     clock_t start = clock(); //checking runtime of the program starting here.
     intro(); //title of the program
     data(); //having different filename give flexibility in accessing different class/department
     
-    FILE*fp;
-    fp = fopen(&filename, "r"); //incompatible integer to pointer conversion passing 'char' to parameter of type 'const char *'; take the address with &
+    FILE*fp = fopen(&filename, "r"); //incompatible integer to pointer conversion passing 'char' to parameter of type 'const char *'; take the address with &
     if(!fp){
         perror("UNABLE TO OPEN FILE: ---> ");//to show error type
-        return 0;
+        return main(); //nice features, to return to main func upon error
     }
     fscanf(fp, "%d", &num_students); //to scan first line of the file
     printf("\nNo. of Student: %d\n", num_students);
     fscanf(fp, "%d", &num_courses); //to scan second line of the file
     printf("No. of Courses: %d\n", num_courses);
-    
+
     while (student_evaluated != num_students){
         student_evaluated += 1; //to close the loop once condition achieved.
-        //printf("Student Evaluated: %d\n", student_evaluated); //testing parameter =, uncomment this if troubleshoot again
-        
-        fscanf(fp, "%ld", &student_id); //to scan third line of the file
+        //printf("\nStudent Evaluated: %d\n", student_evaluated); //testing parameter =, uncomment this if troubleshoot again
+        fscanf(fp, "%ld", &student_id); //to scan third line of the file||using %ld data type long signed.
         printf("\nStudent ID: %ld\n", student_id);
         course = 0; // this to ensure that the course# output does not repeatedly change.
 
         for (x= 0; x < num_courses; x++) { //to iterate the scan based on how many course we have.
             course += 1;//aesthetic for course counter
             fscanf(fp, "%d", &marks);
-            printf("\n\t\tCourse #%d : %d\t\n", course, marks);
-            marking();
-            total = marks+marks; //
-            printf("%d", total); //
-        }
-        total = 0;
+            printf("\n\tCourse #%d : %d\t\n", course, marks);
+            marking(); //call out function for
+            total += marks; //to store marks scanned into total variable
+            y += 1; //use this as trigger for exit condition
 
+            if (y == num_courses){
+                printf("\tTotal Marks = %2.0f\n", total);
+                average_marks = total / num_courses;
+                printf("\tAverage Marks = %.2f\n", average_marks);
+
+                if (average_marks >= 50){
+                    printf("\n\t\t PROCEED TO THE NEXT SEMESTER...\n");
+                }
+                else {
+                    printf("\n\t\t UNFORTUNATELY YOU DID NOT PASS\n\t\tAPPEALING FOR MARK DISABLED IF MARK BELOW 50\n");
+                }
+
+                y = 0; //to reset the trigger
+                total = 0; //to reset the total variable
+            }
+        }
     } 
     fclose(fp);
-
-    
-
     clock_t stop = clock(); //to check how efficient the code is, im using the time.h to monitor the code runtime.
     double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
-    printf("\nTime elapsed in ms: %f\n", elapsed);
+    printf("\nProgram runtime is %.3f ms\n", elapsed);
 }
 
