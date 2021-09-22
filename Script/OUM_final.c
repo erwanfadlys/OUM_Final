ec5a1ba69b244b5f629664f5490dea01cc7aa16c
erwanfadlys <78579279+erwanfadlys@users.noreply.github.com>
2021-09-22 15:11:10 +0800

    ver 1.2
    -Added time to check program runtime
    - Features exporting the result.

--- Script/OUM_final.c
+++ Script/OUM_final.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int num_students, num_courses, x, marks, course, student_evaluated; //x placed there for loop 
+int num_students, num_courses, x, total, marks, course, student_evaluated; //x placed there for loop 
 long int student_id; //to store numbers with 4 bytes
-char filename; //this is where the data name is stored
+char filename, export; //this is where the data name is stored
 
 void intro(){
     printf("SIMPLE GRADING SYSTEM V1.1\n\n");
@@ -30,15 +31,29 @@ void marking(){ //marking function seperated from main, flexibility in coding th
         }
 }
 
-void data(){
+void data(){ //will not be hardcoded instead. Allow for future changes.
     printf("Please enter file name: ");
     scanf("%s", &filename);
 }
 
+void export_file(){  
+    printf("\nExport the file as text?: y / n : ");
+    scanf("%s", &export);
+
+    if(export == 'y'){
+        printf("\nPlease wait for the file to be exported.\nProgram ended.\n");
+        freopen("output.txt","w",stdout);
+    }
+    else{
+        printf("\nProgram ended.\n");
+    }
+}
+
 int main(){
+    clock_t start = clock(); //checking runtime of the program starting here.
     intro(); //title of the program
     data(); //having different filename give flexibility in accessing different class/department
-
+    
     FILE*fp;
     fp = fopen(&filename, "r"); //incompatible integer to pointer conversion passing 'char' to parameter of type 'const char *'; take the address with &
     if(!fp){
@@ -49,10 +64,10 @@ int main(){
     printf("\nNo. of Student: %d\n", num_students);
     fscanf(fp, "%d", &num_courses); //to scan second line of the file
     printf("No. of Courses: %d\n", num_courses);
-
+    
     while (student_evaluated != num_students){
         student_evaluated += 1; //to close the loop once condition achieved.
-        printf("Student Evaluated: %d\n", student_evaluated); //testing parameter 
+        //printf("Student Evaluated: %d\n", student_evaluated); //testing parameter =, uncomment this if troubleshoot again
         
         fscanf(fp, "%ld", &student_id); //to scan third line of the file
         printf("\nStudent ID: %ld\n", student_id);
@@ -63,8 +78,18 @@ int main(){
             fscanf(fp, "%d", &marks);
             printf("\n\t\tCourse #%d : %d\t\n", course, marks);
             marking();
+            total = marks+marks; //
+            printf("%d", total); //
         }
-    }    
+        total = 0;
+
+    } 
     fclose(fp);
+
+    
+
+    clock_t stop = clock(); //to check how efficient the code is, im using the time.h to monitor the code runtime.
+    double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("\nTime elapsed in ms: %f\n", elapsed);
 }
 
